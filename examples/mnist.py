fixed incorrect bias adjustments during backpropagation added mnist example images

examples/mnist.py
@@ -1,7 +1,12 @@
 import gzip
-import numpy as np
+from os import listdir
+from os.path import isfile, join
 
-from neuralnetwork.NeuralNetwork import NeuralNetwork, SigmoidActivation
+import numpy as np
+from PIL import Image
+
+from neuralnetwork.NeuralNetwork import NeuralNetwork, SigmoidActivation, ReLUActivation, TanhActivation, \
+    LeakyReLUActivation
 
 FOLDER = ""
 
@@ -15,6 +20,8 @@ TRAINING_LABELS = "train-labels-idx1-ubyte.gz"
 TEST_SIZE = 10000
 TEST_IMAGES = "t10k-images-idx3-ubyte.gz"
 TEST_LABELS = "t10k-labels-idx1-ubyte.gz"
+
+EXAMPLE_FOLDER = "mnist_test_images"
 
 
 def read_images(path, num_images, image_width, image_height):
@@ -38,8 +45,20 @@ def read_labels(path, num_labels):
     return labels
 
 
+def read_test_images(path):
+    images = [(file, join(path, file)) for file in listdir(path) if isfile(join(path, file))]
+    images = [(file_name, Image.open(image)) for (file_name, image) in images]
+    images = [(file_name, rgb2gray(np.array(image.getdata()))) for (file_name, image) in images]
+
+    return images
+
+
 def normalize_vec(vec, max_value):
     return np.divide(vec, max_value)
+
+
+def rgb2gray(rgb):
+    return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
 
 
 if __name__ == '__main__':
@@ -49,7 +68,7 @@ if __name__ == '__main__':
     test_images = read_images(FOLDER + "/" + TEST_IMAGES, TEST_SIZE, IMAGE_WIDTH, IMAGE_HEIGHT)
     test_labels = read_labels(FOLDER + "/" + TEST_LABELS, TEST_SIZE)
 
-    nn = NeuralNetwork([IMAGE_WIDTH * IMAGE_HEIGHT, 64, 16, 10], activation=SigmoidActivation())
+    nn = NeuralNetwork([IMAGE_WIDTH * IMAGE_HEIGHT, 128, 16, 10], activation=TanhActivation())
 
     # prepare training set
     training_set = list()
@@ -65,14 +84,20 @@ if __name__ == '__main__':
         expected_result[test_labels[test_image_index]] = 1
         test_set.append((normalize_vec(test_images[test_image_index].flatten(), 255), expected_result))
 
-    for iteration in range(0, 500):
+    for iteration in range(0, 2000):
         batch = np.random.randint(0, len(training_set), size=128)
         batch_data = [training_set[index] for index in batch]
 
-        nn.train(batch_data, 10, 0.015, print_debug=False)
+        nn.train(batch_data, 1, 0.15, print_debug=False)
 
         evaluation = np.random.randint(0, len(test_set), size=25)
         evaluation_data = [test_set[index] for index in evaluation]
         print(str(iteration) + ": evaluation result (correct): " + str(nn.evaluate(evaluation_data) / len(evaluation_data)))
 
     print("final evaluation result: " + str(nn.evaluate(test_set)))
+
+    test_images = read_test_images(EXAMPLE_FOLDER)
+    for (file_name, image) in test_images:
+        print("result: (" + str(file_name) + ") " + str(np.argmax(nn.feed_forward(normalize_vec(image, 255)))))
+
+    nn.print()

main.py
@@ -1,10 +1,11 @@
 import numpy as np
 import time
 
-from neuralnetwork.NeuralNetwork import NeuralNetwork
+from neuralnetwork.NeuralNetwork import NeuralNetwork, ReLUActivation, SigmoidActivation, TanhActivation, \
+    LeakyReLUActivation
 
 if __name__ == '__main__':
-    nn = NeuralNetwork([2, 30, 1])
+    nn = NeuralNetwork([2, 30, 1], activation=TanhActivation())
 
     print("feed forward result: " + str(nn.feed_forward(np.asarray([0, 0]))))
     print("feed forward result: " + str(nn.feed_forward(np.asarray([0, 1]))))
@@ -30,3 +31,4 @@ if __name__ == '__main__':
     print("feed forward result: " + str(nn.feed_forward(np.asarray([1, 0]))))
     print("feed forward result: " + str(nn.feed_forward(np.asarray([1, 1]))))
 
+    nn.print()

neuralnetwork/NeuralNetwork.py
@@ -14,6 +14,13 @@ class ActivationFunction(ABC):
     def derivative(self, y):
         pass
 
+    @abc.abstractmethod
+    def initial_weights_range(self, neurons_in, neurons_out) -> (float, float):
+        """
+        :return: (min_value, max_value)
+        """
+        pass
+
 
 class SigmoidActivation(ActivationFunction):
 
@@ -23,14 +30,50 @@ class SigmoidActivation(ActivationFunction):
     def derivative(self, y):
         return y * (1 - y)
 
+    def initial_weights_range(self, neurons_in, neurons_out) -> (float, float):
+        divisor = np.sqrt(neurons_in * neurons_out)
+
+        return -1.0 / divisor, 1.0 / divisor
+
+
+class TanhActivation(ActivationFunction):
+
+    def activate(self, x):
+        return np.tanh(x)
+
+    def derivative(self, y):
+        return 1 - np.power(y, 2)
+
+    def initial_weights_range(self, neurons_in, neurons_out) -> (float, float):
+        divisor = np.sqrt(neurons_in * neurons_out)
+
+        return -1.0 / divisor, 1.0 / divisor
+
 
 class ReLUActivation(ActivationFunction):
 
     def activate(self, x):
-        return max(0, x)
+        return [max(0, value) for value in x]
 
     def derivative(self, y):
-        return 1 if y > 0 else 0
+        return [1 if value > 0 else 0 for value in y]
+
+    def initial_weights_range(self, neurons_in, neurons_out) -> (float, float):
+        r = np.sqrt(12 / (neurons_in + neurons_out))
+        return -r, r
+
+
+class LeakyReLUActivation(ActivationFunction):
+
+    def activate(self, x):
+        return [value if value > 0 else 0.01 * value for value in x]
+
+    def derivative(self, y):
+        return [1 if value > 0 else 0.01 for value in y]
+
+    def initial_weights_range(self, neurons_in, neurons_out) -> (float, float):
+        r = np.sqrt(12 / (neurons_in + neurons_out))
+        return -r, r
 
 
 class NeuralNetwork:
@@ -39,11 +82,15 @@ class NeuralNetwork:
         self.layers = layers
         self.activation = activation
 
-        self.weights = [np.random.uniform(-1.0, 1.0, size=(self.layers[layer], self.layers[layer - 1])) / np.sqrt(self.layers[layer] * self.layers[layer - 1]) for layer in range(1, len(layers))]
+        self.weights = [np.zeros(shape=(self.layers[layer], self.layers[layer - 1])) for layer in range(1, len(self.layers))]
+        for layer in range(1, len(layers)):
+            (initial_min, initial_max) = activation.initial_weights_range(self.layers[layer - 1], self.layers[layer])
+            self.weights[layer - 1] = np.random.uniform(initial_min, initial_max, size=(self.layers[layer], self.layers[layer - 1]))
+
         self.biases = [np.zeros(self.layers[layer]) for layer in range(1, len(layers))]
 
-        self.weight_adjustments = [np.random.rand(self.layers[layer], self.layers[layer - 1]) for layer in range(1, len(layers))]
-        self.bias_adjustments = [np.ones(self.layers[layer]) for layer in range(1, len(layers))]
+        self.weight_adjustments = [np.zeros(shape=(self.layers[layer], self.layers[layer - 1])) for layer in range(1, len(layers))]
+        self.bias_adjustments = [np.zeros(self.layers[layer]) for layer in range(1, len(layers))]
 
         # stores the output of each layer after one forward pass
         self.outputs = [np.zeros(self.layers[layer]) for layer in range(0, len(layers))]
@@ -72,15 +119,14 @@ class NeuralNetwork:
         for layer in range(len(self.layers) - 1, 0, -1):
             deltas = np.multiply(previous_layer_error, self.activation.derivative(self.outputs[layer]))
 
-            np.multiply(learning_rate, np.einsum('i,j->ij', deltas, self.outputs[layer - 1]), out=self.weight_adjustments[layer - 1])
-            np.multiply(learning_rate, deltas, out=self.biases[layer - 1])
-
+            self.weight_adjustments[layer - 1] = learning_rate * np.outer(deltas, self.outputs[layer - 1])
+            self.bias_adjustments[layer - 1] = learning_rate * deltas
             previous_layer_error = self.weights[layer - 1].T @ deltas
 
     def update_weights_and_biases(self):
         for layer in range(0, len(self.layers) - 1):
-            np.subtract(self.weights[layer], self.weight_adjustments[layer], out=self.weights[layer])
-            np.subtract(self.biases[layer], self.bias_adjustments[layer], out=self.biases[layer])
+            self.weights[layer] -= self.weight_adjustments[layer]
+            self.biases[layer] -= self.bias_adjustments[layer]
 
     def train(self, dataset, num_iterations, learning_rate=.015, print_debug=True):
         debug_steps = num_iterations / 25
@@ -107,3 +153,6 @@ class NeuralNetwork:
     def print(self):
         print(self.weights)
         print(self.biases)
+
+    def export(self, file):
+        print(np.array2string(self.weights[0]))